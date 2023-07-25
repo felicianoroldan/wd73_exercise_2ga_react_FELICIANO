b0428bcf70c6f73f28992902e3d4afb2e8900e09
Roldan Feliciano <roldan.feliciano12@gmail.com>
2023-07-26 00:10:54 +0800

    rename: footer to Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Maincontainer from "./components/Maincontainer"
-import Footer from "./components/footer"
+import Footer from "./components/Footer"
 
 
 function App() {
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'animate.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
